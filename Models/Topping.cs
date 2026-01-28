79d81abb7f60343c6c6a9a0d18b70df7e6973d6f
Jhaner Fernandez <jhanerfernandez@gmail.com>
2026-01-28 17:59:20 +0100

    added forms of passing data to child components

---
+++ Models/Topping.cs
@@ -0,0 +1,9 @@
+namespace LearnBlazor.Models
+{
+    // Example for Pizza Topping.cs
+    public class Topping
+    {
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+    }
+}
